c7ac883efd327339b4ef92565edfcbe19981d635
Luca Bello <luca.bello@canonical.com>
2025-02-10 12:27:11 +0100

    pull new prom libraries

--- tests/scenario/test_disable_alerts.py
+++ tests/scenario/test_disable_alerts.py
@@ -9,6 +9,32 @@ import charm
 
 
 @pytest.mark.parametrize("forwarding", (True, False))
+def test_forward_alert_rules_scrape(forwarding):
+    # GIVEN these relations
+    prometheus_relation = Relation("send-remote-write", remote_app_name="prometheus")
+    state = State(
+        leader=True,
+        containers={Container(name="avalanche", can_connect=True)},
+        relations=[
+            prometheus_relation,
+        ],
+        config={"forward_alert_rules": forwarding},
+    )
+    # WHEN the charm receives a config-changed event
+    ctx = Context(
+        charm_type=charm.AvalancheCharm,
+    )
+    with ctx(ctx.on.config_changed(), state) as mgr:
+        output_state = mgr.run()
+        # THEN the charm is forwarding the alerts
+        prometheus_relation_out = output_state.get_relation(prometheus_relation.id)
+        if forwarding:
+            assert prometheus_relation_out.local_app_data["alert_rules"] != "{}"
+        else:
+            assert prometheus_relation_out.local_app_data["alert_rules"] == "{}"
+
+
+@pytest.mark.parametrize("forwarding", (True, False))
 def test_forward_alert_rules(forwarding):
     # GIVEN these relations
     prometheus_relation = Relation("send-remote-write", remote_app_name="prometheus")
@@ -28,4 +54,7 @@ def test_forward_alert_rules(forwarding):
         output_state = mgr.run()
         # THEN the charm is forwarding the alerts
         prometheus_relation_out = output_state.get_relation(prometheus_relation.id)
-        assert prometheus_relation_out.local_app_data != "{}" if forwarding else "{}"
+        if forwarding:
+            assert prometheus_relation_out.local_app_data["alert_rules"] != "{}"
+        else:
+            assert prometheus_relation_out.local_app_data["alert_rules"] == "{}"
