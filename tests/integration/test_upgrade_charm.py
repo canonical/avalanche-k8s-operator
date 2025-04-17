fd16a94dc4a7ac202157f92e1b3171bb04f0f61b
Luca Bello <luca.bello@canonical.com>
2025-04-17 12:13:52 +0200

    fix sh command

--- tests/integration/test_upgrade_charm.py
+++ tests/integration/test_upgrade_charm.py
@@ -10,6 +10,7 @@ import pytest
 import sh
 import yaml
 from helpers import get_config_values  # type: ignore[import]
+from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
 
@@ -18,25 +19,28 @@ app_name = METADATA["name"]
 
 
 @pytest.mark.abort_on_fail
-async def test_config_values_are_retained_after_pod_upgraded(ops_test, charm_under_test):
+async def test_config_values_are_retained_after_pod_upgraded(ops_test: OpsTest, charm_under_test):
     """Deploy from charmhub and then upgrade with the charm-under-test."""
     logger.info("deploy charm from charmhub")
+    assert ops_test.model
     resources = {"avalanche-image": METADATA["resources"]["avalanche-image"]["upstream-source"]}
     resources_arg = f"avalanche-image={resources['avalanche-image']}"
     sh.juju.deploy(  # type: ignore
         app_name,
-        model=ops_test.model,
+        model=ops_test.model.name,
         channel="1/edge",
         base="ubuntu@20.04",
         resource=resources_arg,
     )
 
     config = {"metric_count": "33", "value_interval": "99999"}
-    await ops_test.model.applications[app_name].set_config(config)
+    await ops_test.model.applications[app_name].set_config(config)  # type: ignore
     await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
 
     logger.info("upgrade deployed charm with local charm %s", charm_under_test)
-    sh.juju.refresh(app_name, model=ops_test.model, path=charm_under_test, resource=resources_arg)  # type: ignore
+    sh.juju.refresh(  # type: ignore
+        app_name, model=ops_test.model.name, path=charm_under_test, resource=resources_arg
+    )
     await ops_test.model.wait_for_idle(apps=[app_name], status="active", timeout=1000)
 
     assert (await get_config_values(ops_test, app_name)).items() >= config.items()
