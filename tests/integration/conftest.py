724dde0e04d2b0fa606f31a8c97256663113d9d3
Luca Bello <luca.bello@canonical.com>
2025-08-11 11:30:19 +0200

    cleanup tests

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -2,16 +2,37 @@
 # See LICENSE file for licensing details.
 import os
 from pathlib import Path
+from typing import Dict
 
+import jubilant
 import pytest
-from pytest_operator.plugin import OpsTest
+import sh
+import yaml
 
 
 @pytest.fixture(scope="module")
-async def charm_under_test(ops_test: OpsTest):
+async def charm():
     """Charm used for integration testing."""
     if charm_file := os.environ.get("CHARM_PATH"):
         return Path(charm_file)
 
-    charm = await ops_test.build_charm(".")
+    charm = sh.charmcraft.pack()  # type: ignore
+    assert charm
     return charm
+
+
+@pytest.fixture(scope="module")
+def charm_resources(metadata_file="charmcraft.yaml") -> Dict[str, str]:
+    with open(metadata_file, "r") as file:
+        metadata = yaml.safe_load(file)
+    resources = {}
+    for res, data in metadata["resources"].items():
+        resources[res] = data["upstream-source"]
+    return resources
+
+
+@pytest.fixture(scope="module")
+def juju():
+    keep_models: bool = os.environ.get("KEEP_MODELS") is not None
+    with jubilant.temp_model(keep=keep_models) as juju:
+        yield juju
--- tests/integration/test_remote_write.py
+++ tests/integration/test_remote_write.py
@@ -1,40 +1,14 @@
 #!/usr/bin/env python3
 # Copyright 2021 Canonical Ltd.
 # See LICENSE file for licensing details.
-
-
-import logging
-from pathlib import Path
-
+import jubilant
 import pytest
-import yaml
-from helpers import IPAddressWorkaround  # type: ignore[import]
-
-log = logging.getLogger(__name__)
-
-METADATA = yaml.safe_load(Path("./charmcraft.yaml").read_text())
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test, charm_under_test):
-    """Deploy the charm-under-test and deploy it together with related charms."""
-    await ops_test.model.set_config({"logging-config": "<root>=WARNING; unit=DEBUG"})
-
-    # deploy charm from local source folder
-    resources = {"avalanche-image": METADATA["resources"]["avalanche-image"]["upstream-source"]}
-    await ops_test.model.deploy(charm_under_test, resources=resources, application_name="av")
-    # the charm should go into blocked status until related to alertmanager
-    await ops_test.model.wait_for_idle(apps=["av"], status="active")
-
-
-@pytest.mark.abort_on_fail
-async def test_charm_successfully_relates_to_prometheus(ops_test):
-    # deploy prometheus
-    async with IPAddressWorkaround(ops_test):
-        await ops_test.model.deploy(
-            "ch:prometheus-k8s", application_name="prom", channel="edge", trust=True
-        )
-        await ops_test.model.wait_for_idle(apps=["prom"], status="active")
-
-    await ops_test.model.add_relation("prom:receive-remote-write", "av:send-remote-write")
-    await ops_test.model.wait_for_idle(apps=["av", "prom"], status="active")
+async def test_avalanche_remote_writes_to_prometheus(juju: jubilant.Juju, charm, charm_resources):
+    """Deploy the avalanche and deploy it together with related charms."""
+    juju.deploy(charm, "avalanche", resources=charm_resources)
+    juju.deploy("prometheus-k8s", "prometheus", channel="2/edge", trust=True)
+    juju.integrate("avalanche:send-remote-write", "prometheus:receive-remote-write")
+    juju.wait(jubilant.all_active)
