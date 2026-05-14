1c54c7ebc439ba8b89c38db1c58bd51fb61d8c54
Mateusz Kulewicz <mateusz.kulewicz@canonical.com>
2026-05-14 16:50:34 +0200

    chore: Remove mesh references

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,14 +6,11 @@ output "provides" {
   value = {
     metrics_endpoint  = "metrics-endpoint"
     grafana_dashboard = "grafana-dashboard"
-    provide_cmr_mesh  = "provide-cmr-mesh"
   }
 }
 
 output "requires" {
   value = {
     send_remote_write = "send-remote-write"
-    service_mesh      = "service-mesh"
-    require_cmr_mesh  = "require-cmr-mesh"
   }
 }
